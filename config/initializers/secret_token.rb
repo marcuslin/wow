db23f5050331d7ab97f7cb1a870c337411118240
marcus <marcuslin@marcuss-MacBook-Air.local>
2013-01-23 17:26:31 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wow::Application.config.secret_token = '64f01a934755985d8f8ca802aafcc9de9ad2a3d2a24b83a75a0375066dc0c08d0daf358f98fadc432e59461427d4c84ba746ba917e42bbfe4dd349ecf4f4aca6'
