78f7459053b332358cccda995ddcbe43f46df589
marcuslin <marcuslin0724@gmail.com>
2013-07-04 10:56:23 +0800

    equip_data_error

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
-  $(".equip_data").click(function() {
+  $(".equip_data").click(function(e) {
+    e.preventDefault();
     var equipId = $(this).attr('id');
 
     $.ajax('/equipment/' + equipId + '.json', {
       datatype: 'json',
       success: function(result) {
-        var equip = jQuery.parseJSON(result);
-        console.log(equip);
+
+        var equipStat = $.each(result.equip_stat, function(v) {
+          alert.log(v.stat);
+        });
+
+        var html = "<table id='test'><tr><td class='name'>" + result.equip_name + "</td></tr><tr><td>" + result.equip_class + "</td></tr><tr><td>" + equipStat + "</td></tr></table>";
+
+        $('#equip_details').html(html);
       }
     });
 
