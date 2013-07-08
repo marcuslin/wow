c3d06361fe1fe22f133696bf4e5082d87c7bc437
marcuslin <marcuslin0724@gmail.com>
2013-07-08 10:01:03 +0800

    equip_details_table_styling_problem

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,15 +3,17 @@ $(document).ready(function() {
     e.preventDefault();
     var equipId = $(this).attr('id');
 
+    stats = ''
     $.ajax('/equipment/' + equipId + '.json', {
       datatype: 'json',
       success: function(result) {
 
-        var equipStat = $.each(result.equip_stat, function(v) {
-          alert.log(v.stat);
+        var equipStat = $.each(jQuery.parseJSON(result.equip_stat), function(k, v) {
+          stats += "<p>" + "stat:" + v.stat + ", amount:" + v.amount + "</p>"
+          console.log(stats);
         });
 
-        var html = "<table id='test'><tr><td class='name'>" + result.equip_name + "</td></tr><tr><td>" + result.equip_class + "</td></tr><tr><td>" + equipStat + "</td></tr></table>";
+        var html = "<table id='test'><tr><td class='name'>" + result.equip_name + "</td></tr><tr><td>" + result.equip_class + "</td></tr><tr><td>" + stats + "</td></tr></table>";
 
         $('#equip_details').html(html);
       }
