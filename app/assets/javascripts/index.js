4fa6280b83969c4d8e8ea74e9c5ebea9e0e43fee
cyanglee <cyoung.lee@gmail.com>
2013-07-17 10:19:54 +0800

    add twitter bootstrap tooltip

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
-  $( "#user" ).tooltip({
-    items: "[equip-id]",
-    content: function() {
-      var element = $(this);
-      return element.attr('id');
-    }
+  $("#user").tooltip({
+    selector: "a[equip-id]"
   });
-  // $(".equip_data").click(function(e) {
-  //
-  //   var equipId = $(this).attr('id');
+  // $( "#user" ).tooltip({
+  //   items: "[equip-id]",
+  //   content: function() {
+  //     var stats = '';
+  //     var html = '';
+  //     var element = $(this);
+  //     var equipId = element.attr('id');
 
-  //   stats = ''
+  //     $.ajax('/equipment/' + equipId + '.json', {
+  //       datatype: 'json',
+  //       success: function(result) {
+  //         var equipStat = $.each(result.new_equip_stat, function(k, v){
+  //           stats += "<tr><td>" + v + "</td></tr>";
+  //         });
 
-  //   $.ajax('/equipment/' + equipId + '.json', {
-  //     datatype: 'json',
-  //     success: function(result) {
+  //         html = "<div id='equip_details'><table id='test'><tr><td class='name'>" + result.equip_name + "</td></tr>" + stats + "</table></div>";
+  //         console.log(html);
+  //       }
+  //     });
 
-  //       var equipStat = $.each(result.new_equip_stat, function(k, v){
-  //         stats += "<tr><td>" + v + "</td></tr>"
-  //       });
+  //     return html;
 
-  //       var html = "<table id='test'><tr><td class='name'>" + result.equip_name + "</td></tr>" + stats + "</table>";
-
-  //       $('#equip_details').html(html);
-  //     }
-  //   });
+  //   }
   // });
 });
